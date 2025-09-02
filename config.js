e4224dc389c669baa86dab3c9c0af2b5b10b2373
Limon BD <limonbdtech2020@gmail.com>
2025-09-02 13:31:17 +0600

    update market

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   BOT_TOKEN: process.env.BOT_TOKEN || '8361067153:AAHnm_IFgbwcowCAXAseiLn1HaqIuKjFW2c',
   WEBHOOK_URL:"https://teligram-bot-node.onrender.com",
   GROUP_CHAT_ID:-1002430084000,
-  FINNHUB_KEY:d2r8euhr01qlk22s5c90d2r8euhr01qlk22s5c9g,
+  FINNHUB_KEY:'d2r8euhr01qlk22s5c90d2r8euhr01qlk22s5c9g ',
   TOXICITY_THRESHOLD: 0.9,
   SENTIMENT_THRESHOLD: -2,
   WARN_LIMIT: 1, // প্রথমবার warning
