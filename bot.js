99e500c741f76d532e568e606b8952b2eb8fec9e
Limon BD <limonbdtech2020@gmail.com>
2025-09-02 14:09:08 +0600

    update market

--- bot.js
+++ bot.js
@@ -154,8 +154,8 @@ async function sendMarketUpdate() {
 }
 
 // প্রতি ৫ মিনিটে টেস্টিং জন্য
-cron.schedule('*/5 * * * *', sendMarketUpdate);
-
+// cron.schedule('*/5 * * * *', sendMarketUpdate);
+// 
 // পরে production এ ১ ঘন্টা interval করতে চাইলে:
 // cron.schedule('0 * * * *', sendMarketUpdate);
 
